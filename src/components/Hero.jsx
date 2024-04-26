c78417184ab0b863580903aa4786692d912b0efd
Malmi Withanage <157707365+malmiwithanage@users.noreply.github.com>
2024-04-26 17:52:02 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/", "_blank")
+            window.open("https://github.com/malmiwithanage", "_blank")
           }
           className='black_btn'
         >
